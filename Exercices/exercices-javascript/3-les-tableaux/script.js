a396faee60de20d3a236eda46516520d818e8a81
zaidun <35617206+zaidun@users.noreply.github.com>
2020-06-28 09:21:20 +0200

    update exercices tableaux

--- Exercices/exercices-javascript/3-les-tableaux/script.js
+++ Exercices/exercices-javascript/3-les-tableaux/script.js
@@ -14,15 +14,24 @@ console.log(castArray.indexOf("Robin"))
 // Exercice 4: retire "Robin" du tableau
 castArray.shift();
 // Exercice 5: retire "Ted" du tableau
-
+castArray.pop();
 // Exercice 6: ajoute "Ted" et "Robin" au tableau
-
+castArray.push("Ted","Robin" );
 // Exercice 7: Affiche le nouvel index de Ted et Robin dans le tableau dans la console
-
+console.log(castArray.indexOf("Ted") ,castArray.indexOf("Robin") );
 // Exercice 8: à l'aide d'une boucle, affiche chaque élément du tableau dans la console
-
+castArray.forEach(Element => 
+    console.log(Element)
+)
 // Exercice 9: à l'aide de la méthode .findIndex(), trouve l'index de Lily, ensuite, affiche Lily dans la console
-
+indexOfLily = (element)=>element=="Lily" ;
+console.log(castArray[castArray.findIndex(indexOfLily)]);
 // Exercice 10: à l'aide d'une boucle, multiplie par 2 chaque élément du tableau suivant, crée un nouveau tableau à l'aide de la méthode .push(), affiche le nouveau tableau dans la console
 
 let multiplicationArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+NewArray= [] ;
+multiplicationArray.forEach(element => {
+    NewArray.push(element * 2);
+});
+console.log(NewArray);
